092c07f3d1df682bab7ec4f05e46c051f1651278
chromedays <haik0925@gmail.com>
2024-01-27 15:22:04 -0800

    working on timelines

--- GGJ2024/Assets/Scripts/BossTriggerVolume.cs
+++ GGJ2024/Assets/Scripts/BossTriggerVolume.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
@@ -17,7 +18,7 @@ public class BossTriggerVolume : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (triggered)
+        if (triggered && !Boss.IsDestroyed())
         {
             float speed = 3f;
             float step = speed * Time.deltaTime; // calculate distance to move
@@ -29,18 +30,5 @@ public class BossTriggerVolume : MonoBehaviour
     {
         Debug.Log("Triggered");
         triggered = true;
-        
-
-        
-
-
-#if false
-        // Check if the position of the cube and sphere are approximately equal.
-        if (Vector3.Distance(Boss.position, transform.position) < 0.001f)
-        {
-            // Swap the position of the cylinder.
-            target.position *= -1.0f;
-        }
-#endif
     }
 }
